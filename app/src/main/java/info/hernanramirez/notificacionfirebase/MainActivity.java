3f785474829f04a2100d628101104f956efe0d68
J. Hernan Ramirez R <hr@hernanramirez.info>
2018-07-24 07:41:37 -0400

    Primer commti con notificaicon sencilla

---
+++ app/src/main/java/info/hernanramirez/notificacionfirebase/MainActivity.java
@@ -0,0 +1,40 @@
+package info.hernanramirez.notificacionfirebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lanzarNotificacion(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri sonido = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notificacion)
+                .setContentTitle("Notificacion")
+                .setContentText("Esta es la notificación")
+                .setSound(sonido)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                ;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification.build());
+    }
+}
